5c00154d5d36ac67edc15e4c7358f8f5f07530d8
Lemucya <lemucya@mail.ru>
2022-10-02 15:15:54 +0700

    Написали программу для решения задачи 29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 29: Напишите программу, в которой пользователь задает длину массива, 
+элементы которого задаются в диапазоне [1,99] и выводит на экран .*/
+
+Console.WriteLine("Введите длину массива от 1 до 99");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[arraySize];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Ваш массив: ");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,99);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
